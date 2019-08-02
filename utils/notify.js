6d22982210c8c19f57a6013507466bca8c8b79c6
Tim Arney <tim@line37.com>
2019-08-02 09:29:24 -0400

    add notify client

--- utils/notify.js
+++ utils/notify.js
@@ -1,32 +1,32 @@
+const winston = require("../config/winston.config");
 const NotifyClient = require("notifications-node-client").NotifyClient;
-
 const sendNotification = (req, res, next) => {
-  const templateId = "064c0939-ffa6-46cc-8da7-53aa4081f14e";
-  const liveKey = "-";
+  const data = req.session;
 
-  const notifyClient = new NotifyClient(liveKey);
-
+  const templateId = process.env.TEMPLATE_ID;
+  const key = process.env.API_KEY;
+  const baseUrl = process.env.API_BASE_URL;
+  const notifyClient = new NotifyClient(baseUrl, key);
   const item = {
-    email: "-",
-    first_name: "tim",
-    link: "https://notification.cdssandbox.xyz/register"
+    email: data.personal.email,
+    first_name: data.personal.firstName
   };
 
-  /*
   notifyClient
     .sendEmail(templateId, item.email, {
       personalisation: {
-        first_name: item.first_name,
-        click_here: item.link
+        name: item.first_name
       },
       reference: "Sign Up"
     })
-    .then(response => console.log(response.body))
-    .catch(err => console.log(err.message));
-  */
-
-  //console.log(req.session.confirmCode.code);
-  return next();
+    .then(response => {
+      winston.info(response.body);
+      return next();
+    })
+    .catch(err => {
+      winston.error(err.message);
+      return next();
+    });
 };
 
 module.exports = {
