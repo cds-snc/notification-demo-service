b76b3c5e1edb19142a09031cbcc8e4daf182e1b5
Tim Arney <tim@line37.com>
2019-09-03 14:22:48 -0400

    add login

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,13 +1,12 @@
-
 // create an array of routes to allow us to
 // a) easily chanage the route paths
 // b) add the ability to lookup previous and next routes
 
-
 // 1) add your route(s) here ⬇️
 const routes = [
-  { name: "sample", path: "/sample" }, // remove this
+  // { name: "sample", path: "/sample" }, // remove this
   { name: "start", path: "/start" },
+  { name: "login", path: "/login" },
   { name: "personal", path: "/personal" },
   { name: "confirmation", path: "/confirmation" }
 ];
@@ -15,10 +14,11 @@ const routes = [
 // 2) Add the path to the route controller here ⬇️ ️️
 const configRoutes = app => {
   app.set("appRoutes", routes);
-  
+
   // 🔥
-  require("../routes/_sample_route/sample.controller")(app); // remove this
+  // require("../routes/_sample_route/sample.controller")(app); // remove this
   require("../routes/start/start.controller")(app);
+  require("../routes/login/login.controller")(app);
   require("../routes/personal/personal.controller")(app);
   require("../routes/confirmation/confirmation.controller")(app);
   require("../routes/global/global.controller")(app);
